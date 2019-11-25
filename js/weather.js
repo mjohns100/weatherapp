583b203f6d3a11ca1e2658724c83d9db3fc32496
Meghan Johns <mjohns@madisoncollege.edu>
2019-11-25 16:05:01 -0600

    project is finished

--- js/weather.js
+++ js/weather.js
@@ -10,8 +10,9 @@ $("#search_btn").click(function() {
     //variables for the Openweather API url 
     var apiLink = 'https://api.openweathermap.org/data/2.5/weather';
     var apiKey = 'ea08fa1fca5f57e7b00beaebb6d51069';
+    var apiUnits = '&units=imperial';
     var zipInput = $('#zipCode').val();
-    var apiURL = apiLink + '?zip=' + zipInput + '&appid=' + apiKey;
+    var apiURL = apiLink + '?zip=' + zipInput + '&appid=' + apiKey + apiUnits;
     
     console.log(apiURL);
     
@@ -19,11 +20,37 @@ $("#search_btn").click(function() {
         console.log(responseFromOW);
         console.log("temp:", responseFromOW.main.temp);
         console.log("city:", responseFromOW.name);
-        console.log("description:", responseFromOW.weather[0].description);
+        console.log("description:", responseFromOW.weather[0].main);
         
         var temp = responseFromOW.main.temp;
         var city = responseFromOW.name;
-        var description = responseFromOW.weather[0].description; 
+        var description = responseFromOW.weather[0].main;
         
+    $('#cityName').html("Guess what<br>" + responseFromOW.name + "?");
+    $('#smIts').html("It's");
+    $('#tempF').html(Math.round(responseFromOW.main.temp) + "&deg;");
+    $('#weatherType').html("& " + description);
+
+
+    if (description === "Clear") {
+            $('#bgPhoto').css("background-image", "url(/images/clear.jpg)");
+    } else if (description === "Clouds") {
+            $('#bgPhoto').css("background-image", "url(/images/cloudy.jpg)");
+    } else if (description === "Drizzle") {
+            $('#bgPhoto').css("background-image", "url(/images/rain.jpg)");
+    } else if (description === "Rain") {
+            $('#bgPhoto').css("background-image", "url(/images/rain.jpg)"); 
+    } else if (description === "Thunderstorm") {
+            $('#bgPhoto').css("background-image", "url(/images/storm.jpg)");  
+    } else if (description === "Snow") {
+            $('#bgPhoto').css("background-image", "url(/images/snowing.jpg)");  
+    } else if (description === "Mist") {
+            $('#bgPhoto').css("background-image", "url(/images/foggy.jpg)");  
+    } else if (description === "Fog") {
+            $('#bgPhoto').css("background-image", "url(/images/foggy.jpg)");  
+    } else if (description === "Tornado") {
+            $('#bgPhoto').css("background-image", "url(/images/tornado.jpg)");  
+        }
+   
     });
 });
